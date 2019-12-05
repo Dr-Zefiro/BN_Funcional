62e50c5b44adf08d3991a348f79783a197e5b9c5
Pietro <pietro@coolshop.it>
2019-12-05 21:01:13 +0100

    more stuff

--- functional_helpers.hpp
+++ functional_helpers.hpp
@@ -1,19 +1,48 @@
 #include <vector>
 #include <functional>
+#include <string>
+#include <iostream>
+#include <utility>
+#include <map>
+#include <queue>
+#include <numeric>
+#include "tinyxml2/tinyxml2.h"
+#include "boost/lexical_cast.hpp"
+#include "boost/range/algorithm/transform.hpp"
+#include "boost/algorithm/string.hpp"
+#include "boost/algorithm/algorithm.hpp"
 
-template<typename Function, typename IN, typename OUT = std::decay_t<std::invoke_result_t<Function &, IN const &>>>
-static auto map(Function &&fn, const std::vector<IN> &inputVector) {
-    std::vector<OUT> res;
+namespace B = boost;
+namespace T = tinyxml2;
+namespace S = std;
+
+template<typename Function, typename IN, typename OUT = S::decay_t<S::invoke_result_t<Function &, IN const &>>>
+static auto map(Function &&fn, const S::vector<IN> &inputVector) {
+    S::vector<OUT> res;
     res.reserve(inputVector.size());
     for (auto &&el : inputVector) res.push_back(fn(el));
     return res;
 }
 
-
 template<typename Function, typename T>
-static std::vector<T> filter(const Function &fn, const std::vector<T> &inputVector) {
-    std::vector<T> res;
+static S::vector<T> filter(const Function &fn, const S::vector<T> &inputVector) {
+    S::vector<T> res;
     res.reserve(inputVector.size());
-    for (auto &&el : inputVector) if(fn(el)) res.push_back(el);
+    for (auto &&el : inputVector) if (fn(el)) res.push_back(el);
     return res;
+}
+
+template<typename T>
+static auto printVector(S::vector<T> v){
+    for(auto el: v) S::cout << el << ' ';
+    S::cout << '\n';
+}
+
+static auto splitString(S::string raw, const S::string &delimiter = " ") {
+    using namespace boost::algorithm;
+    S::vector<S::string> tokens;
+    B::algorithm::trim(raw);
+    return raw.empty()
+           ? S::vector<S::string>{}
+           : split(tokens, raw, is_any_of(delimiter));
 }
--- networkLoader.h
+++ networkLoader.h
@@ -6,6 +6,9 @@
 #include <vector>
 #include <map>
 #include <queue>
+#include <numeric>
+#include <random>
+
 #include "functional_helpers.hpp"
 #include "tinyxml2/tinyxml2.h"
 #include "boost/lexical_cast.hpp"
@@ -17,89 +20,165 @@ namespace B = boost;
 namespace T = tinyxml2;
 namespace S = std;
 
-static auto splitString(S::string raw, const S::string &delimiter = " ") {
-    S::vector<S::string> tokens;
-    B::trim(raw);
-    return raw.empty() ? S::vector<S::string>{} : B::split(tokens, raw, B::is_any_of(delimiter));
-}
+typedef size_t nodeState;
 
-auto getAttrId = [](T::XMLElement *s) { return S::string{s->Attribute("id")}; };
+const auto getAttrId = [](T::XMLElement *s) { return S::string{s->Attribute("id")}; };
 
 const auto getToken = [](const char *field, T::XMLElement *node, bool nullable = false) {
-    auto p = node->FirstChildElement(field);
+    const auto p{node->FirstChildElement(field)};
     return (nullable && !p) ? S::vector<S::string>{} : splitString(p->GetText());
 };
 
 auto toVector(T::XMLElement *rootElement, const char *name = nullptr) {
     S::vector<T::XMLElement *> elements{};
-    for (auto el = rootElement->FirstChildElement(name); el; el = el->NextSiblingElement(name))
+    for (auto el{rootElement->FirstChildElement(name)}; el; el = el->NextSiblingElement(name))
         elements.emplace_back(el);
     return elements;
 }
 
+struct CumulativeCpt {
+    S::vector<size_t> pRadix;
+    size_t lineSize{0};
+    S::vector<float> cumulativeCpt;
+
+    CumulativeCpt() = default;
+
+    CumulativeCpt(const S::vector<float> &cpt, const S::vector<size_t> &parentStatusSizes) :
+            pRadix(getRadix(parentStatusSizes, cpt.size())),
+            lineSize(pRadix.empty() ? cpt.size() : pRadix.back()),
+            cumulativeCpt(getCumulativeCPT(cpt, lineSize)) {}
+
+    static S::vector<size_t> getRadix(const S::vector<size_t> &parentStatusSizes, size_t total) {
+        S::vector<size_t> res{};
+        for (const auto p : parentStatusSizes) {
+            total /= p;
+            res.emplace_back(total);
+        }
+        return res;
+    };
+
+    static std::vector<float> getCumulativeCPT(S::vector<float> cpt, const size_t lineSize) {
+        for (auto line{cpt.begin()}; line < cpt.end(); line += lineSize)
+            S::partial_sum(line, line + lineSize, line);
+        return cpt;
+    }
+
+    size_t getState(const S::vector<size_t> &pStatus, const float sample) {
+        const auto offset{S::inner_product(pStatus.begin(), pStatus.end(), pRadix.begin(), size_t{})};
+        const auto line{cumulativeCpt.begin() + offset};
+        return S::upper_bound(line, line + lineSize, sample) - line;
+    };
+};
+
 struct RawNode {
     S::string id;
     S::vector<S::string> stateIds;
-    S::vector<S::string> parentIds;
+    S::vector<S::string> parentIdentifiers;
     S::vector<RawNode *> parents;
 
     S::vector<float> cpt;
-    S::vector<float> cumulativeCpt;
+    CumulativeCpt cumulativeCpt;
 
-    size_t depth = 0;
+    size_t depth{0};
 
     explicit RawNode(T::XMLElement *node) :
             id(getAttrId(node)),
             cpt(map(B::lexical_cast<float, S::string>, getToken("probabilities", node))),
             stateIds(map(getAttrId, toVector(node, "state"))),
-            parentIds(getToken("parents", node, true)) {}
+            parentIdentifiers(getToken("parents", node, true)) {}
 };
 
-auto getXmlNodes(const S::string &name) {
-    auto *doc = new T::XMLDocument{};
-    doc->LoadFile(name.c_str());
-    auto xmlNodes = doc->FirstChildElement("smile")->FirstChildElement("nodes");
+struct BN_Network{
+    S::vector<RawNode *> &nodes;
 
-    return toVector(xmlNodes, "cpt");
-}
+    static auto getXmlNodes(const S::string &name) {
+        auto *doc{new T::XMLDocument{}};
+        doc->LoadFile(name.c_str());
+        const auto xmlNodes{doc->FirstChildElement("smile")->FirstChildElement("nodes")};
 
-void setParents(S::vector<RawNode *> &nodes) {
-    S::map<S::string, RawNode *> nodeMap;
-    for (const auto n : nodes)
-        nodeMap.insert(S::pair<S::string, RawNode *>{n->id, n});
-
-    for (const auto n : nodes)
-        n->parents = map([&nodeMap](const auto p) { return nodeMap.find(p)->second; }, n->parentIds);
-}
-
-void setMaxDepth(const S::vector<RawNode *>& roots,
-                 const S::map<RawNode *, S::vector<RawNode *>> &adjacencyMap,
-                 const size_t depth = 0) {
-    assert(adjacencyMap.size() >= depth);
-    for (const auto root : roots) {
-        root->depth = S::max(depth, root->depth);
-        const auto edges = adjacencyMap.find(root);
-        if (edges == adjacencyMap.end()) continue;
-        setMaxDepth(edges->second, adjacencyMap, depth + 1);
+        return toVector(xmlNodes, "cpt");
     }
-}
 
-auto setLayers(const S::vector<RawNode *> &nodes) {
-    S::vector<S::pair<RawNode *, RawNode *>> edges{};
-    for (const auto node : nodes)
-        for (const auto parent : node->parents)
-            edges.emplace_back(S::make_pair(parent, node));
+    void setCumulativeCPTs() const {
+        for (const auto n : nodes)
+            n->cumulativeCpt = CumulativeCpt(n->cpt, map([](auto p) { return p->stateIds.size(); }, n->parents));
+    }
 
-    S::map<RawNode *, S::vector<RawNode *>> adjacencyMap{};
-    for (const auto edge : edges) adjacencyMap[edge.first].push_back(edge.second);
+    static void setMaxDepth(const S::vector<RawNode *> &roots,
+                     const S::map<RawNode *, S::vector<RawNode *>> &adjacencyMap,
+                     const size_t depth = 0) {
+        assert(adjacencyMap.size() >= depth);
+        for (const auto root : roots) {
+            root->depth = S::max(depth, root->depth);
+            const auto edges{adjacencyMap.find(root)};
+            if (edges == adjacencyMap.end()) continue;
+            setMaxDepth(edges->second, adjacencyMap, depth + 1);
+        }
+    }
 
-    const auto roots = filter([](const auto node){return node->parents.empty();}, nodes);
-    setMaxDepth(roots, adjacencyMap);
-}
+    static void setParents(S::vector<RawNode *> &nodes) {
+        S::map<S::string, RawNode *> nodeMap;
+        for (const auto n : nodes)
+            nodeMap.insert(S::pair<S::string, RawNode *>{n->id, n});
 
-void importBN(const S::string &name) {
-    auto rawNodes = map([](auto node) { return new RawNode{node}; }, getXmlNodes(name));
-    setParents(rawNodes);
-    setLayers(rawNodes);
-}
+        for (const auto n : nodes)
+            n->parents = map([&nodeMap](const auto p) { return nodeMap.find(p)->second; }, n->parentIdentifiers);
+    }
+
+    auto setLayers() const {
+        S::vector<S::pair<RawNode *, RawNode *>> edges{};
+        for (const auto node : nodes)
+            for (const auto parent : node->parents)
+                edges.emplace_back(S::make_pair(parent, node));
+
+        S::map<RawNode *, S::vector<RawNode *>> adjacencyMap{};
+        for (const auto edge : edges) adjacencyMap[edge.first].push_back(edge.second);
+
+        const auto roots{filter([](const auto node) { return node->parents.empty(); }, nodes)};
+        setMaxDepth(roots, adjacencyMap);
+    }
+
+    static S::vector<size_t> sample(){
+        std::random_device rd;
+        std::uniform_real_distribution<> dist{0.0, 1.0};
+
+        auto nodeStates = S::vector<size_t>(nodes.size());
+        auto nodeIterator = nodeStates.begin();
+
+        for(const auto n : nodes){
+            const auto val = dist(rd);
+            const auto pStates = map([&](const auto p) -> size_t {return nodeStates.at(p - nodes[0]); }, n->parents);
+            const auto state = n->cumulativeCpt.getState(pStates, val);
+            *nodeIterator = state;
+            nodeIterator++;
+        }
+        return nodeStates;
+    }
+
+    void importBN(const S::string &name) {
+        auto rawNodes{map([](auto node) { return new RawNode{node}; }, getXmlNodes(name))};
+        setParents(rawNodes);
+        setLayers(rawNodes);
+        setCumulativeCPTs(rawNodes);
+        for(auto i{0}; i < 10; i++)
+            printVector(sample(rawNodes));
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
